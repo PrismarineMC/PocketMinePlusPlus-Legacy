a5407a8510d718a283c9e8e0503dcdcd87cef7ef
a_r_a_d_a_m <mixailenko5@mail.ru>
2015-11-26 18:23:44 +0200

    blockAddress off

--- src/raklib/server/SessionManager.php
+++ src/raklib/server/SessionManager.php
@@ -303,7 +303,7 @@ class SessionManager{
     }
 
     public function blockAddress($address, $timeout = 300){
-        $final = \microtime(\true) + $timeout;
+        /*$final = \microtime(\true) + $timeout;
         if(!isset($this->block[$address]) or $timeout === -1){
             if($timeout === -1){
                 $final = \PHP_INT_MAX;
@@ -313,7 +313,7 @@ class SessionManager{
             $this->block[$address] = $final;
         }elseif($this->block[$address] < $final){
             $this->block[$address] = $final;
-        }
+        }*/
     }
 
     /**
