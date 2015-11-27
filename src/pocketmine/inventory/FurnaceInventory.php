d1153b1e1c1894a0437305c5f051eab0b02c4da7
a_r_a_d_a_m <mixailenko5@mail.ru>
2015-11-27 20:46:51 +0200

    Update...

--- src/pocketmine/inventory/FurnaceInventory.php
+++ src/pocketmine/inventory/FurnaceInventory.php
@@ -21,73 +21,63 @@
 
 namespace pocketmine\inventory;
 
-
 use pocketmine\item\Item;
-use pocketmine\tile\Furnace;
+use pocketmine\Server;
+use pocketmine\utils\UUID;
 
-class FurnaceInventory extends ContainerInventory{
-	public function __construct(Furnace $tile){
-		parent::__construct($tile, InventoryType::get(InventoryType::FURNACE));
+class FurnaceRecipe implements Recipe{
+
+	private $id = \null;
+
+	/** @var Item */
+	private $output;
+
+	/** @var Item */
+	private $ingredient;
+
+	/**
+	 * @param Item $result
+	 * @param Item $ingredient
+	 */
+	public function __construct(Item $result, Item $ingredient){
+		$this->output = clone $result;
+		$this->ingredient = clone $ingredient;
+	}
+
+	public function getId(){
+		return $this->id;
+	}
+
+	public function setId(UUID $id){
+		if($this->id !== \null){
+			throw new \InvalidStateException("Id is already set");
+		}
+
+		$this->id = $id;
 	}
 
 	/**
-	 * @return Furnace
+	 * @param Item $item
 	 */
-	public function getHolder(){
-		return $this->holder;
+	public function setInput(Item $item){
+		$this->ingredient = clone $item;
+	}
+
+	/**
+	 * @return Item
+	 */
+	public function getInput(){
+		return clone $this->ingredient;
 	}
 
 	/**
 	 * @return Item
 	 */
 	public function getResult(){
-		return $this->getItem(2);
+		return clone $this->output;
 	}
 
-	/**
-	 * @return Item
-	 */
-	public function getFuel(){
-		return $this->getItem(1);
-	}
-
-	/**
-	 * @return Item
-	 */
-	public function getSmelting(){
-		return $this->getItem(0);
-	}
-
-	/**
-	 * @param Item $item
-	 *
-	 * @return bool
-	 */
-	public function setResult(Item $item){
-		return $this->setItem(2, $item);
-	}
-
-	/**
-	 * @param Item $item
-	 *
-	 * @return bool
-	 */
-	public function setFuel(Item $item){
-		return $this->setItem(1, $item);
-	}
-
-	/**
-	 * @param Item $item
-	 *
-	 * @return bool
-	 */
-	public function setSmelting(Item $item){
-		return $this->setItem(0, $item);
-	}
-
-	public function onSlotChange($index, $before){
-		parent::onSlotChange($index, $before);
-
-		$this->getHolder()->scheduleUpdate();
+	public function registerToCraftingManager(){
+		Server::getInstance()->getCraftingManager()->registerFurnaceRecipe($this);
 	}
 }
