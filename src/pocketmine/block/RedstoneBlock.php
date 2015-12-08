bf64b84f92f8fee56bb2fed5352bcfccecbccc53
a_r_a_d_a_m <mixailenko5@mail.ru>
2015-12-08 17:38:40 +0200

    Rename Redstone to RedstoneBlock

--- src/pocketmine/block/RedstoneBlock.php
+++ src/pocketmine/block/RedstoneBlock.php
@@ -24,7 +24,7 @@ namespace pocketmine\block;
 use pocketmine\item\Item;
 use pocketmine\item\Tool;
 
-class Redstone extends Solid{
+class RedstoneBlock extends Solid{
 
 	protected $id = self::REDSTONE_BLOCK;
 
